3a66e0f7b3f40b08b6c9aa5220b1c59a144d948c
Thiago Sanches <tsigwt@gmail.com>
2023-07-23 14:29:43 -0300

    Rename the file

--- rp2040-emulator/build.sh
+++ rp2040-emulator/build.sh
@@ -5,7 +5,7 @@ truncate fat12.img -s 1M
 mkfs.vfat -F12 -S512 fat12.img
 mkdir -p img/
 sudo mount -o loop fat12.img fat12/
-sudo cp -ra ../lib/ ../sprites/ ../code.py fat12/
+sudo cp -ra ../lib/ ../sprites/ ../main.py fat12/
 sudo umount fat12/
 du -ha fat12.img
 md5sum fat12.img
