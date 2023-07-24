132be52188531e734140ec13c5497d0227d2e5b7
Thiago Sanches <tsigwt@gmail.com>
2023-07-24 19:37:50 -0300

    Add a simple event manager

--- lib/helpers/EventManager.py
+++ lib/helpers/EventManager.py
@@ -6,6 +6,7 @@ class EventManager:
         self.last_time_read = -1
         self.showing_message = False
         self.live_time = 30
+        self.period_check = 60
 
     def get(self):
         with open(f"/event.json", "r") as read_file:
