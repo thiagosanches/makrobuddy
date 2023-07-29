64fefe4185cc9940f6082e3a6e14a4b44da53500
Thiago Sanches <tsigwt@gmail.com>
2023-07-29 06:22:44 -0300

    Update a little bit the deploy.sh script to minify some json file

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,14 @@ mkdir -p "/media/$USER/CIRCUITPY/lib"
 mkdir -p "/media/$USER/CIRCUITPY/sprites"
 mkdir -p "/media/$USER/CIRCUITPY/fonts"
 
+# 'minifing' a little bit the json config files.
+find sprites/ -name '*.json' -exec cp {} {}.tmp \;
+find sprites/ -name '*.tmp' -print0 | xargs -0 -I {} bash -c 'NEW_NAME=$(echo {} | sed 's/tmp/min/g'); cat {} | tr "\n" " " | sed "s/ //g" > $NEW_NAME'
+find sprites/ -name '*.tmp' -exec rm -rf {} \;
+
 rsync -avhru --delete "lib" "/media/$USER/CIRCUITPY"
-rsync -avhru --delete "fonts" "/media/$USER/CIRCUITPY"
-rsync -avhru --delete "sprites" "/media/$USER/CIRCUITPY"
+rsync -avhru --ignore-existing "fonts" "/media/$USER/CIRCUITPY"
+rsync -avhru --delete --exclude "*.json" "sprites" "/media/$USER/CIRCUITPY"
 rsync -avhru --delete "main.py" "/media/$USER/CIRCUITPY"
 
 # synchronize cached writes to persistent storage.
