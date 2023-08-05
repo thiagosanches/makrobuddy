dd2399861f652898bbda14198a5da698c1366626
Thiago Sanches <tsigwt@gmail.com>
2023-08-05 05:42:17 -0300

    Refactor on RotaryEncoder class

--- lib/makrobuddy/eletronic_components/RotaryEncoder.py
+++ lib/makrobuddy/eletronic_components/RotaryEncoder.py
@@ -1,40 +1,25 @@
-import time
 import board
-import os
 import usb_hid
 import rotaryio
 import digitalio
-import supervisor
 
-from lib.makrobuddy.helpers.TextManager import TextManager
-from lib.makrobuddy.helpers.SpriteManager import SpriteManager
-from lib.makrobuddy.helpers.Display import Display
-from lib.makrobuddy.helpers.EventManager import EventManager
-from lib.makrobuddy.eletronic_components.RotaryEncoder import RotaryEncoder
-
-
-from digitalio import DigitalInOut, Direction, Pull
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keycode import Keycode
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 class RotaryEncoder:
     def __init__(self) -> None:
-        self.encoder_button = digitalio.DigitalInOut(board.GP22)
-        self.encoder_button.direction = digitalio.Direction.INPUT
-        self.encoder_button.pull = digitalio.Pull.UP
-        self.encoder_button_state = None
-
         self.encoder = rotaryio.IncrementalEncoder(board.GP20, board.GP21)
-        self.last_position = self.encoder.position
-
-        
+        self.encoder_last_position = self.encoder.position       
         self.cc = ConsumerControl(usb_hid.devices)
+        
+        self.button = digitalio.DigitalInOut(board.GP22)
+        self.button.direction = digitalio.Direction.INPUT
+        self.button.pull = digitalio.Pull.UP
+        self.button_state = None       
 
-    def on_tick(self):    
+    def run(self):    
         current_position = self.encoder.position
-        position_change = current_position - last_position
+        position_change = current_position - self.encoder_last_position
         if position_change > 0:
             for _ in range(position_change):
                 self.cc.send(ConsumerControlCode.VOLUME_INCREMENT)
@@ -43,10 +28,10 @@ class RotaryEncoder:
             for _ in range(-position_change):
                 self.cc.send(ConsumerControlCode.VOLUME_DECREMENT)
             print(current_position)
-        last_position = current_position
+        self.encoder_last_position = current_position
 
-        if not self.encoder_button.value and self.encoder_button_state is None:
+        if not self.button.value and self.button_state is None:
             self.encoder_button_state = "pressed"
-        if self.encoder_button.value and self.encoder_button_state == "pressed":
+        if self.button.value and self.button_state == "pressed":
             self.cc.send(ConsumerControlCode.PLAY_PAUSE)
-            self.encoder_button_state = None
+            self.button_state = None
